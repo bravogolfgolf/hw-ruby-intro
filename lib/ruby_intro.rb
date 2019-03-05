c03968f397261fd589b44ae57839023dbca5620d
Brian Gibson <gibsonbg@me.com>
2019-03-05 18:29:11 +0000

    Completes hello and starts_with_consonant?

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -21,11 +21,15 @@ end
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  'Hello, ' + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  starts_with_vowel = /^[aeiouAEIOU]/
+  return false if (s =~ starts_with_vowel)
+  
+  starts_with_letter = /^[a-zA-Z]/
+  return true if (s =~ starts_with_letter)
 end
 
 def binary_multiple_of_4? s
